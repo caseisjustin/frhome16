74f857fce0f3f3d7c01326e5f758a4d8fa43e02d
hello <helloworld@gmail.com>
2024-12-05 13:15:27 +0000

    hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../component/Header"
+import Hero from "../../component/Hero";
 
 const Home = () => {
 
     return (
         <>
             <Header/>
+            <Hero/>
         </>
     )
 }
