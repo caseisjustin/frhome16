8f9802dc99a50e0747cf38428b14d7698d8394b2
hello <helloworld@gmail.com>
2024-12-05 12:32:40 +0000

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 
@@ -7,6 +7,8 @@ function App() {
 
 	return (
 		<>
+		<Link to={"/"}>home</Link>
+		<Link to={"/about"}>About</Link>
 			<Routes>
 				<Route path='/' element={<Home/>}/>
 				<Route path='/about' element={<About/>}/>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Header from "../../component/Header"
 
 const Home = () => {
 
     return (
-        <div id="home">
-            Home
-        </div>
+        <>
+            <Header/>
+        </>
     )
 }
 
